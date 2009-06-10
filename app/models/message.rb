f0812553f9cdcf3eb3e5f6fb061705e12c1d00c3
Daniel Jackoway <danjdel@gmail.com>
2009-06-10 16:46:19 -0500

    added message.pending named scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
   belongs_to :user
 
+  named_scope :pending, :conditions => {:listened => false}
+
   def filepath_no_extension
     filepath.include?('.') ? filepath[0...(filepath.rindex('.'))] : filepath
   end
